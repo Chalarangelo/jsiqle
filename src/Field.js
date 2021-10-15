518cd6599ccd019df72d060c41f23db89264c14e
Chalarangelo <chalarangelo@gmail.com>
2021-10-15 19:28:00 +0300

    Tighter Field setup

--- src/Field.js
+++ src/Field.js
@@ -1,36 +1,5 @@
-import validators from './typeValidation.js';
-
-const isNil = val => val === null || val === undefined;
-
-const parseType = type => {
-  if (typeof type === 'string') return validators[type];
-  if (typeof type === 'object') {
-    const typeKeys = Object.keys(type);
-    if (typeKeys.length !== 1) throw new Error('Invalid type object');
-    const typeKey = typeKeys[0];
-    const typeValue = type[typeKey];
-    if (typeKey === 'oneOf') {
-      const types = typeValue.map(parseType);
-      return validators.oneOf(...types);
-    } else if (typeKey === 'enum') {
-      return validators.enum(...typeValue);
-    } else if (typeKey === 'arrayOf') {
-      return validators.arrayOf(parseType(typeValue));
-    } else if (typeKey === 'objectOf') {
-      return validators.objectOf(parseType(typeValue));
-    } else if (typeKey === 'object') {
-      const keys = Object.keys(typeValue);
-      const shape = keys.reduce((acc, key) => {
-        acc[key] = parseType(typeValue[key]);
-        return acc;
-      }, {});
-      return validators.object(shape);
-    } else {
-      throw new Error('Invalid type object');
-    }
-  }
-  throw new Error('Type must be a string or object');
-};
+import validators from './utils/typeValidation.js';
+import isValidName from './utils/validateName.js';
 
 class Field {
   constructor(
@@ -39,30 +8,29 @@ class Field {
       defaultValue: null,
     }
   ) {
-    if (!name || typeof name !== 'string')
-      throw new Error('Field name is required');
-    if (!type) throw Error('Field type is required');
-    if (required && !isNil(defaultValue))
-      throw Error('Field cannot be required and have a default value');
+    // Verify name
+    const [validName, error] = isValidName(name);
+    if (!validName) throw `Field name ${error}.`;
     this.name = name;
-    this.type = type;
-    this.validators = parseType(type);
-    this.required = required;
+
+    // Set required
+    this.required = Boolean(required);
+
+    // Verify type, wrap in optional if not required
+    if (typeof type !== 'function')
+      throw new Error('Field type must be a function.');
+    this.type = this.required ? type : validators.optional(type);
+
+    // Ensure default value is of correct type and not null or undefined if required
+    if (this.required && validators.nil(defaultValue))
+      throw new Error('Default value cannot be null or undefined.');
+    if (!this.validate(defaultValue))
+      throw new Error('Default value must be valid.');
     this.defaultValue = defaultValue;
   }
 
   validate(value) {
-    return this.validators(value);
-  }
-
-  toString() {
-    return `${this.name}: ${JSON.stringify(this.type)}${
-      this.required ? ' (required)' : ' (optional)'
-    }${
-      !isNil(this.defaultValue)
-        ? ` (default: ${JSON.stringify(this.defaultValue)})`
-        : ''
-    }`;
+    return this.typeValidator(value);
   }
 }
 

f082dca8ef7ede0d71905ffea94f02e960eba5e2
Chalarangelo <chalarangelo@gmail.com>
2021-10-23 21:07:08 +0300

    Handle fields, methods, keys and records

--- src/Model.js
+++ src/Model.js
@@ -1,85 +1,167 @@
 import QMap from './QMap.js';
+import Field from './Field.js';
+import { symbolize } from './utils/symbols.js';
+import { deepClone } from './utils/deepClone.js';
+import isValidName from './utils/validateName.js';
+import validators from './utils/typeValidation.js';
+import { RecordHandler, Record } from './recordHelper.js';
 
-const $keyPrefix = Symbol('keyPrefix');
-const $keyCounter = Symbol('keyCounter');
-const $objects = Symbol('objects');
-const $requiredFields = Symbol('requiredFields');
-const $optionalFields = Symbol('optionalFields');
+const $fields = symbolize('fields');
+const $key = symbolize('key');
+const $methods = symbolize('methods');
+const $records = symbolize('records');
+const $recordHandler = symbolize('recordHandler');
 
 class Model {
   constructor(
     {
       name,
       fields,
-      // methods,
+      key,
+      methods,
+      // scopes,
       // relations,
-      // options,
       // hooks,
       // validations,
       // indexes,
-      // plugins,
-      // ...rest
     } = {
       fields: [],
+      methods: [],
     }
   ) {
-    if (!name || typeof name !== 'string')
-      throw new Error('Model name is required');
+    // Verify name
+    const [validName, error] = isValidName(name);
+    if (!validName) throw `Model name ${error}.`;
     this.name = name;
 
-    this.fields = fields || [];
-    this[$requiredFields] = this.fields.filter(field => field.required);
-    this[$optionalFields] = this.fields.filter(field => !field.required);
+    // Create the record storage and handler
+    // This needs to be initialized before fields to allow for retrofilling
+    this[$records] = new QMap();
+    this[$recordHandler] = new RecordHandler(this);
 
-    this[$keyPrefix] = `[${this.name}]:#`;
-    this[$keyCounter] = 0;
+    // Add fields, checking for duplicates and invalids
+    this[$fields] = new Map();
+    fields.forEach(this.addField);
 
-    this[$objects] = new QMap();
+    // Check and create the key field
+    if (!this[$fields].has(key))
+      throw new Error(`Model ${this.name} has no key field ${key}.`);
+    if (!this[$fields].get(key).required)
+      throw new Error(`Model ${this.name} key field ${key} is not required.`);
+    this[$key] = key;
+
+    // Add methods, checking for duplicates and invalids
+    this[$methods] = new Map();
+    methods.forEach(this.addMethod);
   }
 
-  get nextKey() {
-    this[$keyCounter] += 1;
-    return `${this[$keyPrefix]}${this[$keyCounter]}`;
+  addField(field, retrofill) {
+    if (!(field instanceof Field))
+      throw new Error(`Field ${field} is not a Field.`);
+    if (this[$fields].has(field.name))
+      throw new Error(`Duplicate field name ${field.name}.`);
+
+    this[$fields].add(field.name, field);
+
+    // Retrofill records with new fields
+    const isRetrofillFunction = typeof retrofill === 'function';
+    this[$records].forEach(record => {
+      record[field.name] = isRetrofillFunction ? retrofill(record) : retrofill;
+      if (!field.validate(record[field.name])) {
+        throw new Error(
+          `${this.name} record has invalid value for field ${field.name}.`
+        );
+      }
+    });
   }
 
-  add(object) {
-    if (!object) throw new Error('Object is required');
+  removeField(name) {
+    if (!this[$fields].has(name))
+      throw new Error(`Field ${name} does not exist.`);
 
-    // TODO: Deep clone the object first?
-    this[$requiredFields].forEach(field => {
-      if (object[field.name] === undefined)
+    this[$fields].delete(name);
+  }
+
+  updateField(name, field) {
+    if (!(field instanceof Field))
+      throw new Error(`Field ${field} is not a Field.`);
+    this.removeField(name);
+    this.addField(field);
+  }
+
+  addMethod(name, method) {
+    if (typeof method !== 'function')
+      throw new Error(`Method ${name} is not a function.`);
+    if (this[$methods].has(name))
+      throw new Error(`Method ${name} already exists.`);
+
+    this[$methods].set(name, method);
+  }
+
+  removeMethod(name) {
+    if (!this[$methods].has(name))
+      throw new Error(`Method ${name} does not exist.`);
+
+    this[$methods].delete(name);
+  }
+
+  add(record) {
+    if (!record) throw new Error('Record is required');
+
+    const newRecordKey = record[this[$key]];
+    if (newRecordKey === undefined)
+      throw new Error(`${this.name} record has no key.`);
+    if (this[$records].has(newRecordKey))
+      throw new Error(
+        `${this.name} record with key ${newRecordKey} already exists.`
+      );
+
+    const newRecord = deepClone(record);
+    const newRecordFields = new Set(...Object.keys(newRecord));
+    this[$fields].forEach(field => {
+      const isFieldNil = validators.isNil(newRecord[field.name]);
+      // Set the default value if the field is null or undefined
+      if (field.required && isFieldNil)
+        newRecord[field.name] = field.defaultValue;
+      // Throw an error if the field value is invalid
+      if (!field.validate(newRecord[field.name])) {
         throw new Error(
-          `${this.name} object is missing required field ${field.name}`
+          `${this.name} record has invalid value for field ${field.name}.`
         );
-      if (!field.validate(object[field.name]))
-        throw new Error(
-          `${this.name} object has invalid value for field ${field.name}`
-        );
+      }
+      newRecordFields.delete(field.name);
     });
 
-    this[$optionalFields].forEach(field => {
-      if (
-        object[field.name] !== undefined &&
-        !field.validate(object[field.name])
-      )
-        throw new Error(
-          `${this.name} object has invalid value for field ${field.name}`
-        );
-      if (object[field.name] === undefined)
-        object[field.name] = field.defaultValue;
-    });
+    if (newRecordFields.size > 0)
+      console.warn(
+        `${this.name} record has extra fields: ${[...newRecordFields].join(
+          ', '
+        )}.`
+      );
 
-    const key = this.nextKey;
-    this[$objects].set(key, object);
-    return object;
-  }
-
-  get objects() {
-    return this[$objects];
+    this[$records].set(newRecordKey, newRecord);
+    return new Record(newRecord, this[$recordHandler]);
   }
 
   get(key) {
-    return this[$objects].get(key);
+    return new Record(this[$records].get(key), this[$recordHandler]);
+  }
+
+  delete(key) {
+    if (!this[$records].has(key))
+      throw new Error(`${this.name} record with key ${key} does not exist.`);
+
+    this[$records].delete(key);
+  }
+
+  get records() {
+    return this[$records].map(
+      object => new Record(object, this[$recordHandler])
+    );
+  }
+
+  clear() {
+    this[$records].clear();
   }
 }
 

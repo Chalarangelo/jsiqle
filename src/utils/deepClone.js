6d3ecdf8bc51592bc0ad887057639db54cdbad4c
Chalarangelo <chalarangelo@gmail.com>
2021-10-23 21:05:14 +0300

    Add deepClone utility

---
+++ src/utils/deepClone.js
@@ -0,0 +1,19 @@
+/**
+ * Creates a deep clone of an object.
+ * @param {Object} obj - The object to clone.
+ * @returns {Object} The cloned object.
+ */
+export const deepClone = obj => {
+  if (obj === null) return null;
+  let clone = Object.assign({}, obj);
+  Object.keys(clone).forEach(
+    key =>
+      (clone[key] =
+        typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key])
+  );
+  if (Array.isArray(obj)) {
+    clone.length = obj.length;
+    return Array.from(clone);
+  }
+  return clone;
+};

c10219f4821ec9d14e804f25896f260c77f08df2
Chalarangelo <chalarangelo@gmail.com>
2021-10-24 20:58:19 +0300

    Install webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'node',
+  resolve: {
+    alias: {
+      src: [path.resolve(__dirname, 'src')],
+    },
+  },
+};

df56e2dbf4d712a91eed9d21aec5a100b4f613aa
Chalarangelo <chalarangelo@gmail.com>
2021-10-24 21:25:45 +0300

    Setup Jest with Babel
    Install babel dependencies.
    Add a configuration for Babel.
    Setup aliases in Babel.

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          src: './src',
+        },
+      },
+    ],
+  ],
+};

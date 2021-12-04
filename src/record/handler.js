898e517449a86d197f5eac2ddbe86aa6c1a29567
Chalarangelo <chalarangelo@gmail.com>
2021-12-04 14:17:07 +0200

    Internalize handler utlities

--- src/record/handler.js
+++ src/record/handler.js
@@ -20,24 +20,30 @@ const {
 } = symbols;
 
 class RecordHandler {
+  #model;
+
   constructor(model) {
-    this.model = model;
+    this.#model = model;
+  }
+
+  get model() {
+    return this.#model;
   }
 
   createRecord(recordData) {
     if (!recordData) throw new TypeError('Record data cannot be empty.');
-    const modelName = this.getModelName();
+    const modelName = this.#getModelName();
     // Validate record key
     const newRecordKey = RecordHandler.#validateNewRecordKey(
       modelName,
-      this.getKey(),
-      recordData[this.getKey().name],
-      this.model.records
+      this.#getKey(),
+      recordData[this.#getKey().name],
+      this.#model.records
     );
     // Clone record data, check for extra properties
     const clonedRecord = deepClone(recordData);
     const extraProperties = Object.keys(clonedRecord).filter(
-      property => !this.hasField(property) && !this.isModelKey(property)
+      property => !this.#hasField(property) && !this.#isModelKey(property)
     );
     if (extraProperties.length > 0) {
       console.warn(
@@ -47,7 +53,7 @@ class RecordHandler {
     // Create record with key and extra properties only
     const newRecord = new Record(
       {
-        [this.getKey().name]: newRecordKey,
+        [this.#getKey().name]: newRecordKey,
         ...extraProperties.reduce(
           (obj, property) => ({ ...obj, [property]: clonedRecord[property] }),
           {}
@@ -56,12 +62,12 @@ class RecordHandler {
       this
     );
     // Set fields and skip validation
-    this.getFieldNames().forEach(field => {
+    this.#getFieldNames().forEach(field => {
       this.set(newRecord, field, clonedRecord[field], newRecord, true);
     });
     // Validate record just once
-    this.getValidators().forEach((validator, validatorName) => {
-      if (!validator(newRecord, this.model.records))
+    this.#getValidators().forEach((validator, validatorName) => {
+      if (!validator(newRecord, this.#model.records))
         throw new RangeError(
           `${modelName} record with key ${newRecordKey} failed validation for ${validatorName}.`
         );
@@ -70,110 +76,25 @@ class RecordHandler {
     return [newRecordKey, newRecord];
   }
 
-  /*  ======  Utility methods  ======  */
-  // TODO: Internalize these!
-
-  getModelName() {
-    return this.model.name;
-  }
-
-  getFieldNames() {
-    return [...this.model[$fields].keys()];
-  }
-
-  getValidators() {
-    return this.model[$validators];
-  }
-
-  isModelKey(property) {
-    return this.model[$key].name === property;
-  }
-
-  getKey() {
-    return this.model[$key];
-  }
-
-  getKeyValue(record) {
-    return record[$recordValue][this.model[$key].name];
-  }
-
-  hasField(property) {
-    return this.model[$fields].has(property);
-  }
-
-  getField(property) {
-    return this.model[$fields].get(property);
-  }
-
-  getFieldValue(record, property) {
-    return record[$recordValue][property];
-  }
-
-  hasMethod(property) {
-    return this.model[$methods].has(property);
-  }
-
-  getMethod(record, property) {
-    return this.model[$methods].get(property)(record[$recordValue]);
-  }
-
-  hasRelationshipField(property) {
-    // A relationship field exists if a field with the same name exists and
-    // a relationship exists named `${property}.${property}`. This is due to
-    // relationships being stored as a `.`-delimited tuple of the relationship
-    // name and the field/method name. In the case of the field name, it's the
-    // same as the actual relationship name.
-    if (!this.hasField(property)) return false;
-    return this.model[$relationships].has(`${property}.${property}`);
-  }
-
-  getRelationship(record, property) {
-    // Get the relationship from the field only. The field name matches that of
-    // the relationship, so the relationship key is ${property}.${property}`.
-    return this.model[$relationships]
-      .get(`${property}.${property}`)
-      [$get](this.getModelName(), property, record[$recordValue]);
-  }
-
-  isCallToSerialize(property) {
-    return property === 'toObject' || property === 'toJSON';
-  }
-
-  isCallToString(property) {
-    return property === 'toString';
-  }
-
-  isKnownSymbol(property) {
-    return [$recordModel, $recordTag, $recordValue, $isRecord, $key].includes(
-      property
-    );
-  }
-
-  getKnownSymbol(record, property) {
-    if (property === $isRecord) return true;
-    if (property === $key) this.getKey();
-    return record[property];
-  }
-
   /*  ======  Trap definitions  ======  */
 
   get(record, property) {
     // Check relationships first to avoid matching them as fields
-    if (this.hasRelationshipField(property))
-      return this.getRelationship(record, property);
+    if (this.#hasRelationshipField(property))
+      return this.#getRelationship(record, property);
     // Key or field, return as-is
-    if (this.isModelKey(property) || this.hasField(property))
-      return this.getFieldValue(record, property);
+    if (this.#isModelKey(property) || this.#hasField(property))
+      return this.#getFieldValue(record, property);
     // Method, get and call, this also matches relationship reverses (methods)
-    if (this.hasMethod(property)) return this.getMethod(record, property);
+    if (this.#hasMethod(property)) return this.#getMethod(record, property);
     // Serialize method, call and return
-    if (this.isCallToSerialize(property))
-      return RecordHandler.#recordToObject(record, this.model, this);
+    if (this.#isCallToSerialize(property))
+      return RecordHandler.#recordToObject(record, this.#model, this);
     // Call toString method, return key value
-    if (this.isCallToString(property)) return this.getKeyValue(record);
+    if (this.#isCallToString(property)) return this.#getKeyValue(record);
     // Known symbol, handle as required
-    if (this.isKnownSymbol(property))
-      return this.getKnownSymbol(record, property);
+    if (this.#isKnownSymbol(property))
+      return this.#getKnownSymbol(record, property);
     // Unknown property, return undefined
     return undefined;
   }
@@ -181,37 +102,42 @@ class RecordHandler {
   set(record, property, value, receiver, skipValidation) {
     // Receiver is the same as record but never used (API compatibility)
     const recordValue = record[$recordValue];
-    const recordKey = this.getKeyValue(record);
-    const otherRecords = this.model.records.except(recordKey);
+    const recordKey = this.#getKeyValue(record);
+    const otherRecords = this.#model.records.except(recordKey);
     // Throw an error when trying to set a method, also catches
     // relationship reverses, safeguarding against issues there.
-    if (this.hasMethod(property))
+    if (this.#hasMethod(property))
       throw new TypeError(
-        `${this.getModelName()} record ${recordKey} cannot set method ${property}.`
+        `${this.#getModelName()} record ${recordKey} cannot set method ${property}.`
       );
     // Validate and set field, warn if field is not defined
-    if (this.hasField(property)) {
-      const field = this.getField(property);
-      RecordHandler.#setRecordField(this.model.name, recordValue, field, value);
+    if (this.#hasField(property)) {
+      const field = this.#getField(property);
+      RecordHandler.#setRecordField(
+        this.#model.name,
+        recordValue,
+        field,
+        value
+      );
       // Never skip individual field validation
       field[$validators].forEach((validator, validatorName) => {
         if (!validator(recordValue, otherRecords))
           throw new RangeError(
-            `${this.getModelName()} record with key ${recordKey} failed validation for ${validatorName}.`
+            `${this.#getModelName()} record with key ${recordKey} failed validation for ${validatorName}.`
           );
       });
     } else {
-      console.warn(`${this.name} record has extra field: ${property}.`);
+      console.warn(`${this.#model.name} record has extra field: ${property}.`);
       recordValue[property] = value;
     }
     // Perform model validations
     // The last argument, `skipValidation`, is used to skip validation
     // and should only ever be set to `true` by the by the handler itself.
     if (!skipValidation) {
-      this.getValidators().forEach((validator, validatorName) => {
+      this.#getValidators().forEach((validator, validatorName) => {
         if (!validator(recordValue, otherRecords))
           throw new RangeError(
-            `${this.getModelName()} record with key ${recordKey} failed validation for ${validatorName}.`
+            `${this.#getModelName()} record with key ${recordKey} failed validation for ${validatorName}.`
           );
       });
     }
@@ -289,6 +215,90 @@ class RecordHandler {
       );
     return newRecordKey;
   };
+
+  /*  ======  Utility methods  ======  */
+
+  #getModelName() {
+    return this.#model.name;
+  }
+
+  #getFieldNames() {
+    return [...this.#model[$fields].keys()];
+  }
+
+  #getValidators() {
+    return this.#model[$validators];
+  }
+
+  #isModelKey(property) {
+    return this.#model[$key].name === property;
+  }
+
+  #getKey() {
+    return this.#model[$key];
+  }
+
+  #getKeyValue(record) {
+    return record[$recordValue][this.#model[$key].name];
+  }
+
+  #hasField(property) {
+    return this.#model[$fields].has(property);
+  }
+
+  #getField(property) {
+    return this.#model[$fields].get(property);
+  }
+
+  #getFieldValue(record, property) {
+    return record[$recordValue][property];
+  }
+
+  #hasMethod(property) {
+    return this.#model[$methods].has(property);
+  }
+
+  #getMethod(record, property) {
+    return this.#model[$methods].get(property)(record[$recordValue]);
+  }
+
+  #hasRelationshipField(property) {
+    // A relationship field exists if a field with the same name exists and
+    // a relationship exists named `${property}.${property}`. This is due to
+    // relationships being stored as a `.`-delimited tuple of the relationship
+    // name and the field/method name. In the case of the field name, it's the
+    // same as the actual relationship name.
+    if (!this.#hasField(property)) return false;
+    return this.#model[$relationships].has(`${property}.${property}`);
+  }
+
+  #getRelationship(record, property) {
+    // Get the relationship from the field only. The field name matches that of
+    // the relationship, so the relationship key is ${property}.${property}`.
+    return this.#model[$relationships]
+      .get(`${property}.${property}`)
+      [$get](this.#getModelName(), property, record[$recordValue]);
+  }
+
+  #isCallToSerialize(property) {
+    return property === 'toObject' || property === 'toJSON';
+  }
+
+  #isCallToString(property) {
+    return property === 'toString';
+  }
+
+  #isKnownSymbol(property) {
+    return [$recordModel, $recordTag, $recordValue, $isRecord, $key].includes(
+      property
+    );
+  }
+
+  #getKnownSymbol(record, property) {
+    if (property === $isRecord) return true;
+    if (property === $key) this.#getKey();
+    return record[property];
+  }
 }
 
 export default RecordHandler;

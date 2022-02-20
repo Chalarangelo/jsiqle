d3fe60cce9a5cbebf4478fa7ea1028d6018ece6c
Chalarangelo <chalarangelo@gmail.com>
2022-02-20 12:13:05 +0200

    refactor: Clean up object type leftovers
    Remove any leftover code related to object type fields. Clean up
    internal exports and definitions for field types. Update usage as
    necessary to use named type exports.
    
    Refs: #38

--- src/types.js
+++ src/types.js
@@ -18,51 +18,16 @@ const or =
 
 const isArrayOf = type => val => Array.isArray(val) && val.every(type);
 
-const isObject = shape => {
-  const props = Object.keys(shape);
-  return val => {
-    if (val === null || val === undefined || typeof val !== 'object')
-      return false;
-    if (props.length === 0) return true;
-    const valProps = Object.keys(val);
-    if (valProps.length !== props.length) return false;
-    return props.every(prop => shape[prop](val[prop]));
-  };
-};
-
-const isObjectOf = type => val => {
-  if (val === null || val === undefined || typeof val !== 'object')
-    return false;
-  return Object.keys(val).every(prop => type(val[prop]));
-};
-
-const isEnum =
+export const isEnum =
   (...values) =>
   val =>
     values.includes(val);
 
 const isNull = val => val === null;
 
-const isUndefined = val => val === undefined;
+export const isUndefined = val => val === undefined;
 
-const isOptional = type => val => or(isNull, type)(val);
-
-export default {
-  // Primitive types
-  bool: isBoolean,
-  number: isNumber,
-  string: isString,
-  date: isDate,
-  // Special types
-  enum: isEnum,
-  // Composition types
-  object: isObject,
-  objectOf: isObjectOf,
-  optional: isOptional,
-  // Empty types
-  null: isNull,
-  undefined: isUndefined,
-};
+export const isOptional = type => val => or(isNull, type)(val);
 
 export const standardTypes = {
   boolean: { type: isBoolean },

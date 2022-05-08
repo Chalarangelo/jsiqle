851e65d3225ac458beb92c77f1191024e363b4b8
Chalarangelo <chalarangelo@gmail.com>
2022-05-08 19:00:46 +0300

    feat: Stop validation of serializer names
    Serializer names should be validated on schema creation.
    Individual serializer validation is not necessary.
    
    Refs: #47

--- src/serializer.js
+++ src/serializer.js
@@ -1,5 +1,4 @@
 import { DuplicationError } from 'src/errors';
-import { validateName } from 'src/utils';
 
 export class Serializer {
   #name;
@@ -7,9 +6,7 @@ export class Serializer {
   #methods;
 
   constructor({ name, attributes = [], methods = {} }) {
-    // TODO: V2 Enhancements
-    // This check here is not necessary. We might be able to get rid of it.
-    this.#name = validateName(name);
+    this.#name = name;
     this.#attributes = new Map();
     this.#methods = new Map();
 

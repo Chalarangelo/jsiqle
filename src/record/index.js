3eb7d6472db7aeaf8b8bfe027755c7652b297bdf
Angelos Chalaris <chalarangelo@gmail.com>
2023-06-16 18:54:04 +0300

    feat!: Alter `RecordSet.prototype.groupBy()`
    Update `RecordSet.prototype.groupBy()` to return an object
    instead of a `RecordGroup`. This simplifies the resulting value
    and operations. This also results in the internally used
    `RecordGroup` class being unused, which is also deprecated
    as a result now. Documentation has been updated accordingly.
    
    BREAKING CHANGE: `RecordSet.prototype.groupBy()` now returns an
    object instead of a `RecordGroup` instance. Each key contains a
    `RecordSet` instance of the grouped records.
    `RecordGroup` instances are no longer produced by any operations.

--- src/record/index.js
+++ src/record/index.js
@@ -1,4 +1,3 @@
 export { default as Record } from './record';
-export { default as RecordGroup } from './group';
 export { default as RecordHandler } from './handler';
 export { default as RecordSet } from './set';
